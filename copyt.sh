3d2fc5970cc83ea97a43b33cc63a7dee2acdcee3
Jens Lee <rxrw@me.com>
2021-08-20 10:43:43 +0800

    适配15.0.1

--- copyt.sh
+++
@@ -1,5 +0,0 @@
-cp ~/IdeaProjects/keycloak-justauth/temp/* ~/tools/keycloak-jb/keycloak/themes/base/admin/resources/partials/
-
-cp -r ~/IdeaProjects/keycloak-justauth/ui/font_iconfont ~/tools/keycloak-jb/keycloak/themes/keycloak/common/resources/lib/
-
-cp ~/IdeaProjects/keycloak-justauth/ui/theme.properties ~/tools/keycloak-jb/keycloak/themes/keycloak/login/
